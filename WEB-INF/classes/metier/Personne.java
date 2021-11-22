77cf4b4276b5fdc071b4e0a6421966c74a43428b
Baptiste hardelin <baptiste.hardelin.etu@univ-lille.fr>
2021-11-22 21:48:11 +0100

    ajout de monObjet.jsp

---
+++ WEB-INF/classes/metier/Personne.java
@@ -0,0 +1,16 @@
+package metier;
+
+public class Personne {
+
+    private int i;
+
+    public Personne() {
+        i = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "C'est le " + i + "ème Hello World !";
+    }
+    
+}
